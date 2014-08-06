b9370c37fa71c97c20c78e1fcad556ad515a2145
Daisuke Oka <daisuke_oka@nanosoftware.biz>
2014-08-06 16:22:58 +0900

    Label追加

--- Hassouseisei2_0/BrainStormingFirstViewController.m
+++ Hassouseisei2_0/BrainStormingFirstViewController.m
@@ -26,15 +26,39 @@
     // Dispose of any resources that can be recreated.
 }
 
--(void)SetUpUserInterfaceGeneralModel
+-(void)SetUpUserInterface_iPadMini_iPad2
 {
-    NSString *model=[[UIDevice currentDevice] model];
-    if([model isEqualToString:@"iPhone"]){
-        //iPhone
-    }
+    
 }
 
--(void)SetUpAtViewDidLoadUsingUILabelToOutputWord
+-(void)SetUpUserInterface_iPadRetina
+{
+    
+}
+
+-(void)SetUpUserInterface_iPhone3_iPhone3S
+{
+    
+}
+
+-(void)SetUpUserInterface_iPhone4_iPhone4S
+{
+    
+}
+
+-(void)SetUpUsetInterface_iPhone5_iPhone5S
+{
+    
+}
+
+-(void)SetUpUserInterface_OtherDevice
+{
+    
+}
+
+
+
+-(void)SetUpUserInterface
 //UILabelを単語をアウトプットするのに使用する時にViewDidLoadで呼び出されるルーチン
 {
     int ScreenHeight = [[UIScreen mainScreen] bounds].size.height;
@@ -45,7 +69,13 @@
     }else if(ScreenHeight == 2048&&ScreenWidth == 1536){
         //iPad Retina
     }else if(ScreenHeight == 480 && ScreenWidth == 320){
-        //
+        //iPhone 3,iPhone 3S
+    }else if(ScreenHeight == 960 && ScreenWidth == 640){
+        //iPhone 4,iPhone 4S
+    }else if(ScreenHeight == 1136 && ScreenWidth == 640){
+        //iPhone 5,iPhone 5S
+    }else{
+        //それ以外の機種
     }
     
     if([[UIScreen mainScreen] bounds].size.height > 480){  //iPhone 5以降の画面サイズの場合
