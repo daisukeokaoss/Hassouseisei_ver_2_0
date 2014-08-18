320fe31620fb2a975b0a979cf4a519507f6f7788
Daisuke Oka <daisuke_oka@nanosoftware.biz>
2014-08-19 03:00:20 +0900

    日本語ワードストア追加

---
+++ Hassouseisei2_0/ChemicalWordStore.h
@@ -0,0 +1,17 @@
+//
+//  ChemicalWordStore.h
+//  BrainStorming
+//
+//  Created by 岡 大輔 on 2013/02/27.
+//  Copyright (c) 2013年 岡 大輔. All rights reserved.
+//
+
+
+//#import "WordStore.h"
+
+
+@interface ChemicalWordStore : NSObject
+
++(NSMutableArray *)GenerateWordArray;
+
+@end
