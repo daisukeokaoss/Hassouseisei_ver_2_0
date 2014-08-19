c119f909c4be91c5f20c2ddd130c71ad4964a4d8
Daisuke Oka <daisuke_oka@nanosoftware.biz>
2014-08-20 00:57:58 +0900

    SwitchのUI作りかけ

--- Hassouseisei2_0/WordSelectionPlotScrollView_iPhone.m
+++ Hassouseisei2_0/WordSelectionPlotScrollView_iPhone.m
@@ -12,10 +12,24 @@
 @interface WordSelectionPlotScrollView_iPhone()
 
 @property (nonatomic,strong) UISegmentedControl *wordCountSegmentedControl;
+
 @property (nonatomic,strong) UISwitch *BiologicalWordSwitch;
 @property (nonatomic,strong) UILabel  *BiologicalWordLabel;
+
 @property (nonatomic,strong) UISwitch *ChemicalWordSwitch;
 @property (nonatomic,strong) UILabel  *ChemicalWordLabel;
+
+@property (nonatomic,strong) UISwitch *ITWordSwitch;
+@property (nonatomic,strong) UILabel  *ITWordLabel;
+
+@property (nonatomic,strong) UISwitch *ModernSocialWordSwitch;
+@property (nonatomic,strong) UILabel  *ModernSocialWordLabel;
+
+@property (nonatomic,strong) UISwitch *OpticalWordSwitch;
+@property (nonatomic,strong) UILabel *OpticalWordLabel;
+
+@property (nonatomic,strong) UISwitch *ElementaryWordSwitch;
+@property (nonatomic,strong) UILabel *ElementaryWordLabel;
 @end
 
 @implementation WordSelectionPlotScrollView_iPhone
@@ -33,15 +47,39 @@
 {
     [self setContentSize:size];
     
-    self.wordCountSegmentedControl = [[UISegmentedControl alloc] initWithFrame:CGRectMake(30,20,250, 29)];
     
-    self.BiologicalWordSwitch = [[UISwitch alloc] initWithFrame:CGRectMake(30, 30,51 , 31)];
-    self.BiologicalWordLabel  = [[UILabel alloc] initWithFrame:CGRectMake(90,30, 68, 21)];
+    NSArray *items = @[@"2",@"3",@"4",@"5",@"6",@"7"];
+    
+    self.wordCountSegmentedControl = [[UISegmentedControl alloc] initWithItems:items];
+    
+    [self.wordCountSegmentedControl setFrame:CGRectMake(30,20,250, 29)];
+    
+    
+    self.BiologicalWordLabel  = [[UILabel alloc] initWithFrame:CGRectMake(36,66, 68, 21)];
+    self.BiologicalWordSwitch = [[UISwitch alloc] initWithFrame:CGRectMake(237,66,51 , 31)];
+    
     [self.BiologicalWordLabel setText:@"生物用語"];
+    [self.BiologicalWordLabel sizeToFit];
     
-    self.ChemicalWordSwitch   = [[UISwitch alloc] initWithFrame:CGRectMake(30,105, 51, 31)];
-    self.ChemicalWordLabel    = [[UISwitch alloc] initWithFrame:CGRectMake(90,105,68, 21)];
+    
+    self.ChemicalWordLabel    = [[UILabel alloc] initWithFrame:CGRectMake(36,105,68, 21)];
+    self.ChemicalWordSwitch   = [[UISwitch alloc] initWithFrame:CGRectMake(237,105, 51, 31)];
+    
     [self.ChemicalWordLabel setText:@"化学用語"];
+    [self.ChemicalWordLabel sizeToFit];
+    
+    self.ITWordLabel         = [[UILabel alloc] initWithFrame:CGRectMake(36,144,49,21)];
+    self.ITWordSwitch        = [[UISwitch alloc] initWithFrame:CGRectMake(237, 144, 51, 31)];
+    [self.ITWordLabel setText:@"IT用語"];
+    [self.ITWordLabel sizeToFit];
+    
+    
+    self.ModernSocialWordLabel = [[UILabel alloc] initWithFrame:CGRectMake(36,183,102, 21)];
+    self.ModernSocialWordSwitch = [[UISwitch alloc] initWithFrame:CGRectMake(237,183,51,31)];
+    [self.ModernSocialWordLabel setText:@"現代社会用語"];
+    [self.ModernSocialWordLabel sizeToFit];
+    
+    
     //ここ、作りかけ
     
     
