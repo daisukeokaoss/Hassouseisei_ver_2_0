ef54423dc6a172e3ad31721b7d0a5f0daa4d29cd
Daisuke Oka <daisuke_oka@nanosoftware.biz>
2014-10-02 15:06:59 +0900

    タグワード実装

---
+++ Hassouseisei2_0/StoreTagWordToiDrive.h
@@ -0,0 +1,15 @@
+//
+//  StoreTagWordToiDrive.h
+//  Hassouseisei2_0
+//
+//  Created by おかやん on 2014/10/02.
+//  Copyright (c) 2014年 ナノソフトウェア. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StoreTagWordToiDrive : NSObject
+
+
+
+@end
---
+++ Hassouseisei2_0/StoreTagWordToiDrive.m
@@ -0,0 +1,20 @@
+//
+//  StoreTagWordToiDrive.m
+//  Hassouseisei2_0
+//
+//  Created by おかやん on 2014/10/02.
+//  Copyright (c) 2014年 ナノソフトウェア. All rights reserved.
+//
+
+#import "StoreTagWordToiDrive.h"
+
+@interface StoreTagWordToiDrive()
+@property (nonatomic,strong) NSMutableArray *TagWordsA;
+@property (nonatomic,strong) NSMutableArray *TagWordsB;
+//ここ、作りかけ
+
+@end
+
+@implementation StoreTagWordToiDrive
+
+@end
